c7134084146539220a19b55104e47739678e118e
st-nhitruong <nhi.truong@supremetech.vn>
2023-04-18 13:39:13 +0700

    exercise 4

---
+++ src/app/__test__/product.cart.test.ts
@@ -0,0 +1,51 @@
+import { Cart, LineProduct } from "./../lib/product.cart";
+
+const prd1: LineProduct = {
+  product: {
+    id: 1,
+    name: "socola",
+    price: 10,
+    discount: [
+      {
+        percent: 5,
+        quatity: 1,
+      },
+      {
+        percent: 10,
+        quatity: 5,
+      },
+    ],
+  },
+  quantity: 1,
+};
+
+describe("test cart product", () => {
+  describe("test add product", () => {
+    const cart = new Cart([]);
+    const addPrdMock = jest.spyOn(Cart.prototype, "getCart");
+
+    cart.addProduct(prd1);
+    test("add product success", () => {
+      expect(cart.getCart()).toHaveLength(1);
+      expect(cart.getCart()).toContain(prd1);
+      expect(addPrdMock).toBeCalled();
+
+    });
+  });
+
+  describe("test remove product in cart", () => {
+    const cart = new Cart([]);
+    cart.removeProduct(1);
+    test("remove product in order success", () => {
+      expect(cart.getCart()).toHaveLength(0);
+      expect(cart.getCart()).toEqual([]);
+    });
+  });
+
+  describe("totalPrice", () => {
+    const cart = new Cart([prd1]);
+    test("calc otalPrice success", () => {
+      expect(cart.totalPrice()).toBe(9.5);
+    });
+  });
+});
---
+++ src/app/lib/product.cart.ts
@@ -0,0 +1,79 @@
+type Discount = {
+  quatity: number;
+  percent: number;
+};
+export type Product = {
+  id: number;
+  name: string;
+  price: number;
+  discount: Discount[];
+};
+
+export type LineProduct = {
+  product: Product;
+  quantity: number;
+};
+
+interface ListProduct {
+  listProduct: LineProduct[];
+}
+[];
+export class Cart implements ListProduct {
+  listProduct: LineProduct[] = [];
+  constructor(data: LineProduct[]) {
+    this.listProduct = data;
+  }
+
+  getCart() {
+    return this.listProduct;
+  };
+
+  // addProduct = (prd: Product) => {
+  //   let index = this.listProduct.findIndex(
+  //     (item) => item.product.id === prd.id
+  //   );
+  //   if (index !== -1) {
+  //     return this.listProduct[index].quantity = +1;
+  //   } else {
+  //     return this.listProduct.push({
+  //       product: prd,
+  //       quantity: 1,
+  //     });
+  //   }
+  // };
+
+  addProduct(prd: LineProduct) {
+    let index = this.listProduct.findIndex(
+      (item) => item.product.id === prd.product.id
+    );
+    if (index !== -1) {
+      return (this.listProduct[index].quantity = +1);
+    } else {
+      return this.listProduct.push(prd);
+    }
+  };
+
+  removeProduct(id: number) {
+    this.listProduct = this.listProduct.filter(
+      (item) => item.product.id !== id
+    );
+  };
+
+  totalPrice() {
+    let calcDiscount = 0;
+    return this.listProduct.reduce((total, lineProduct: LineProduct) => {
+      lineProduct.product.discount.forEach((discount: Discount) => {
+        if (lineProduct.quantity >= discount.quatity) {
+          calcDiscount = discount.percent;
+        }
+      });
+      return (
+        total +
+        (lineProduct.product.price *
+          lineProduct.quantity *
+          (100 - calcDiscount)) /
+          100
+      );
+    }, 0);
+  };
+}
