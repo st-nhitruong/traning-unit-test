57400b1755e2ec344053d86ea9b32a2650ba27ba
st-nhitruong <nhi.truong@supremetech.vn>
2023-04-23 16:57:32 +0700

    fix review

--- src/app/lib/utils/isAscending.ts
+++ src/app/lib/utils/isAscending.ts
@@ -1,9 +1,9 @@
 export const isAscending = (arr) => {
-  if (!Array.isArray(arr) || arr.length < 2 || arr.some(isNaN)) {
+  if (!Array.isArray(arr) || arr.length < 2) {
     return false;
   }
   for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i + 1] < arr[i]) {
+    if (arr[i + 1] < arr[i] || isNaN(arr[i])) {
       return false;
     }
   }
